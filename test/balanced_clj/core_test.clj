95993274bd373f543bdf79b18ba8d96724c60230
Edward Cho <zerokarmaleft@gmail.com>
2014-03-12 16:56:51 -0500

    add tests for API keys

--- test/balanced_clj/core_test.clj
+++ test/balanced_clj/core_test.clj
@@ -1,7 +1,37 @@
 (ns balanced-clj.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test      :refer :all]
+            [vcr-clj.clj-http  :refer [with-cassette]]
             [balanced-clj.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-api-keys
+  (with-cassette :new-api-key do
+    (let [api-key (-> (create-api-key)
+                      (get "api_keys")
+                      first)]
+      (is (not-empty api-key))
+      (is (= #{"links" "created_at" "secret" "href" "meta" "id"}
+             (set (keys api-key))))
+      (is (.startsWith (api-key "secret") "ak-test-"))
+      (is (.startsWith (api-key "href")   "/api_keys")))))
+
+(deftest test-fetch-api-key
+  (with-cassette :existing-api-key do
+    (let [api-key (-> (fetch-api-key "AK4b6Elv2iAqJRpu9lD7H53a")
+                      (get "api_keys")
+                      first)]
+      (is (not-empty api-key))
+      (is (= #{"href" "created_at" "meta" "id" "links"}
+             (set (keys api-key)))))))
+
+(deftest test-list-api-keys
+  (with-cassette :all-api-keys do
+    (let [api-keys (list-api-keys)]
+      (is (not-empty api-keys))
+      (is (= #{"api_keys" "links" "meta"}
+             (set (keys api-keys)))))))
+
+(deftest test-delete-api-key
+  (with-cassette :delete-api-key-resp do
+    (let [resp (delete-api-key "AK4b6Elv2iAqJRpu9lD7H53a")]
+      (is (not-empty resp))
+      (is (= 204 (:status resp))))))
